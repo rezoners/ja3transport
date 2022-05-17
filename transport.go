74848deba471dac1edae854bf734af47258d71ad
rezoners <58162029+rezoners@users.noreply.github.com>
2022-05-17 23:42:21 +0300

    Update transport.go

--- transport.go
+++ transport.go
@@ -196,68 +196,68 @@ func stringToSpec(ja3 string) (*tls.ClientHelloSpec, error) {
 		GetSessionID:       sha256.Sum256,
 	}, nil
 }
-func genMap() (extMap map[string]utls.TLSExtension) {
-	extMap = map[string]utls.TLSExtension{
-		"0": &utls.SNIExtension{},
-		"5": &utls.StatusRequestExtension{},
+func genMap() (extMap map[string]tls.TLSExtension) {
+	extMap = map[string]tls.TLSExtension{
+		"0": &tls.SNIExtension{},
+		"5": &tls.StatusRequestExtension{},
 		// These are applied later
 		// "10": &tls.SupportedCurvesExtension{...}
 		// "11": &tls.SupportedPointsExtension{...}
-		"13": &utls.SignatureAlgorithmsExtension{
-			SupportedSignatureAlgorithms: []utls.SignatureScheme{
-				utls.ECDSAWithP256AndSHA256,
-				utls.ECDSAWithP384AndSHA384,
-				utls.ECDSAWithP521AndSHA512,
-				utls.PSSWithSHA256,
-				utls.PSSWithSHA384,
-				utls.PSSWithSHA512,
-				utls.PKCS1WithSHA256,
-				utls.PKCS1WithSHA384,
-				utls.PKCS1WithSHA512,
-				utls.ECDSAWithSHA1,
-				utls.PKCS1WithSHA1,
+		"13": &tls.SignatureAlgorithmsExtension{
+			SupportedSignatureAlgorithms: []tls.SignatureScheme{
+				tls.ECDSAWithP256AndSHA256,
+				tls.ECDSAWithP384AndSHA384,
+				tls.ECDSAWithP521AndSHA512,
+				tls.PSSWithSHA256,
+				tls.PSSWithSHA384,
+				tls.PSSWithSHA512,
+				tls.PKCS1WithSHA256,
+				tls.PKCS1WithSHA384,
+				tls.PKCS1WithSHA512,
+				tls.ECDSAWithSHA1,
+				tls.PKCS1WithSHA1,
 			},
 		},
-		"16": &utls.ALPNExtension{
+		"16": &tls.ALPNExtension{
 			AlpnProtocols: []string{"h2", "http/1.1"},
 		},
-		"18": &utls.SCTExtension{},
-		"21": &utls.UtlsPaddingExtension{GetPaddingLen: utls.BoringPaddingStyle},
-		"22": &utls.GenericExtension{Id: 22}, // encrypt_then_mac
-		"23": &utls.UtlsExtendedMasterSecretExtension{},
-		"27": &utls.FakeCertCompressionAlgsExtension{
+		"18": &tls.SCTExtension{},
+		"21": &tls.UtlsPaddingExtension{GetPaddingLen: utls.BoringPaddingStyle},
+		"22": &tls.GenericExtension{Id: 22}, // encrypt_then_mac
+		"23": &tls.UtlsExtendedMasterSecretExtension{},
+		"27": &tls.FakeCertCompressionAlgsExtension{
 			Methods: []utls.CertCompressionAlgo{utls.CertCompressionBrotli},
 		},
-		"28": &utls.FakeRecordSizeLimitExtension{}, //Limit: 0x4001
-		"35": &utls.SessionTicketExtension{},
-		"34": &utls.GenericExtension{Id: 34},
-		"41": &utls.GenericExtension{Id: 41}, //FIXME pre_shared_key
-		"43": &utls.SupportedVersionsExtension{Versions: []uint16{
-			utls.GREASE_PLACEHOLDER,
-			utls.VersionTLS13,
-			utls.VersionTLS12,
-			utls.VersionTLS11,
-			utls.VersionTLS10}},
-		"44": &utls.CookieExtension{},
-		"45": &utls.PSKKeyExchangeModesExtension{Modes: []uint8{
-			utls.PskModeDHE,
+		"28": &tls.FakeRecordSizeLimitExtension{}, //Limit: 0x4001
+		"35": &tls.SessionTicketExtension{},
+		"34": &tls.GenericExtension{Id: 34},
+		"41": &tls.GenericExtension{Id: 41}, //FIXME pre_shared_key
+		"43": &tls.SupportedVersionsExtension{Versions: []uint16{
+			tls.GREASE_PLACEHOLDER,
+			tls.VersionTLS13,
+			tls.VersionTLS12,
+			tls.VersionTLS11,
+			tls.VersionTLS10}},
+		"44": &tls.CookieExtension{},
+		"45": &tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+			tls.PskModeDHE,
 		}},
-		"49": &utls.GenericExtension{Id: 49}, // post_handshake_auth
-		"50": &utls.GenericExtension{Id: 50}, // signature_algorithms_cert
-		"51": &utls.KeyShareExtension{KeyShares: []utls.KeyShare{
-			{Group: utls.CurveID(utls.GREASE_PLACEHOLDER), Data: []byte{0}},
-			{Group: utls.X25519},
+		"49": &tls.GenericExtension{Id: 49}, // post_handshake_auth
+		"50": &tls.GenericExtension{Id: 50}, // signature_algorithms_cert
+		"51": &tls.KeyShareExtension{KeyShares: []utls.KeyShare{
+			{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
+			{Group: tls.X25519},
 
 			// {Group: utls.CurveP384}, known bug missing correct extensions for handshake
 		}},
-		"30032": &utls.GenericExtension{Id: 0x7550, Data: []byte{0}}, //FIXME
-		"13172": &utls.NPNExtension{},
-		"17513": &utls.ApplicationSettingsExtension{
+		"30032": &tls.GenericExtension{Id: 0x7550, Data: []byte{0}}, //FIXME
+		"13172": &tls.NPNExtension{},
+		"17513": &tls.ApplicationSettingsExtension{
 			SupportedALPNList: []string{
 				"h2",
 			},
 		},
-		"65281": &utls.RenegotiationInfoExtension{
+		"65281": &tls.RenegotiationInfoExtension{
 			Renegotiation: utls.RenegotiateOnceAsClient,
 		},
 	}
