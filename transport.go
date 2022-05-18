82734556390438382a9230c50bf0771a12debe14
rezoners <58162029+rezoners@users.noreply.github.com>
2022-05-18 15:16:12 +0300

    Update transport.go

--- transport.go
+++ transport.go
@@ -34,48 +34,48 @@ func (e ErrExtensionNotExist) Error() string {
 // used to calculate the JA3 signature. These JA3-dependent values are applied
 // after the instantiation of the map.
 var extMap = map[string]tls.TLSExtension{
-		"0": &tls.SNIExtension{},
-		"5": &tls.StatusRequestExtension{},
-		// These are applied later
-		// "10": &tls.SupportedCurvesExtension{...}
-		// "11": &tls.SupportedPointsExtension{...}
-		"13": &tls.SignatureAlgorithmsExtension{
-			SupportedSignatureAlgorithms: []tls.SignatureScheme{
-				tls.ECDSAWithP256AndSHA256,
-				tls.ECDSAWithP384AndSHA384,
-				tls.ECDSAWithP521AndSHA512,
-				tls.PSSWithSHA256,
-				tls.PSSWithSHA384,
-				tls.PSSWithSHA512,
-				tls.PKCS1WithSHA256,
-				tls.PKCS1WithSHA384,
-				tls.PKCS1WithSHA512,
-				tls.ECDSAWithSHA1,
-				tls.PKCS1WithSHA1,
-			},
+	"0": &tls.SNIExtension{},
+	"5": &tls.StatusRequestExtension{},
+	// These are applied later
+	// "10": &tls.SupportedCurvesExtension{...}
+	// "11": &tls.SupportedPointsExtension{...}
+	"13": &tls.SignatureAlgorithmsExtension{
+		SupportedSignatureAlgorithms: []tls.SignatureScheme{
+			tls.ECDSAWithP256AndSHA256,
+			tls.ECDSAWithP384AndSHA384,
+			tls.ECDSAWithP521AndSHA512,
+			tls.PSSWithSHA256,
+			tls.PSSWithSHA384,
+			tls.PSSWithSHA512,
+			tls.PKCS1WithSHA256,
+			tls.PKCS1WithSHA384,
+			tls.PKCS1WithSHA512,
+			tls.ECDSAWithSHA1,
+			tls.PKCS1WithSHA1,
 		},
-		"16": &tls.ALPNExtension{
-			AlpnProtocols: []string{"h2", "http/1.1"},
-		},
-		"18": &tls.SCTExtension{},
-		"21": &tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle},
-		"22": &tls.GenericExtension{Id: 22}, // encrypt_then_mac
-		"23": &tls.UtlsExtendedMasterSecretExtension{},
-		"27": &tls.FakeCertCompressionAlgsExtension{},
-		"28": &tls.FakeRecordSizeLimitExtension{},
-		"35": &tls.SessionTicketExtension{},
-		"44": &tls.CookieExtension{},
-		"45": &tls.PSKKeyExchangeModesExtension{Modes: []uint8{
-			tls.PskModeDHE,
-		}},
-		"49": &tls.GenericExtension{Id: 49}, // post_handshake_auth
-		"50": &tls.GenericExtension{Id: 50}, // signature_algorithms_cert
-		"51": &tls.KeyShareExtension{KeyShares: []tls.KeyShare{{Group: tls.X25519},
-			{Group: tls.CurveP256}}},
-		"13172": &tls.NPNExtension{},
-		"65281": &tls.RenegotiationInfoExtension{
-			Renegotiation: tls.RenegotiateOnceAsClient,
-		},
+	},
+	"16": &tls.ALPNExtension{
+		AlpnProtocols: []string{"h2", "http/1.1"},
+	},
+	"18": &tls.SCTExtension{},
+	"21": &tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle},
+	"22": &tls.GenericExtension{Id: 22}, // encrypt_then_mac
+	"23": &tls.UtlsExtendedMasterSecretExtension{},
+	"27": &tls.FakeCertCompressionAlgsExtension{},
+	"28": &tls.FakeRecordSizeLimitExtension{},
+	"35": &tls.SessionTicketExtension{},
+	"44": &tls.CookieExtension{},
+	"45": &tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+		tls.PskModeDHE,
+	}},
+	"49": &tls.GenericExtension{Id: 49}, // post_handshake_auth
+	"50": &tls.GenericExtension{Id: 50}, // signature_algorithms_cert
+	"51": &tls.KeyShareExtension{KeyShares: []tls.KeyShare{{Group: tls.X25519},
+		{Group: tls.CurveP256}}},
+	"13172": &tls.NPNExtension{},
+	"65281": &tls.RenegotiationInfoExtension{
+		Renegotiation: tls.RenegotiateOnceAsClient,
+	},
 }
 
 // NewTransport creates an http.Transport which mocks the given JA3 signature when HTTPS is used
@@ -113,7 +113,7 @@ func NewTransportWithConfig(ja3 string, config *tls.Config) (*http.Transport, er
 
 // stringToSpec creates a ClientHelloSpec based on a JA3 string
 func stringToSpec(ja3 string) (*tls.ClientHelloSpec, error) {
-	extMap := genMap()
+	extMap := genMap2()
 	tokens := strings.Split(ja3, ",")
 
 	version := tokens[0]
@@ -196,6 +196,77 @@ func stringToSpec(ja3 string) (*tls.ClientHelloSpec, error) {
 		GetSessionID:       sha256.Sum256,
 	}, nil
 }
+
+
+func genMap2() (extMap map[string]tls.TLSExtension) {
+	extMap = map[string]tls.TLSExtension{
+		"0": &tls.SNIExtension{},
+		"5": &tls.StatusRequestExtension{},
+		// These are applied later
+		// "10": &tls.SupportedCurvesExtension{...}
+		// "11": &tls.SupportedPointsExtension{...}
+		"13": &tls.SignatureAlgorithmsExtension{
+			SupportedSignatureAlgorithms: []tls.SignatureScheme{
+				tls.ECDSAWithP256AndSHA256,
+				tls.ECDSAWithP384AndSHA384,
+				tls.ECDSAWithP521AndSHA512,
+				tls.PSSWithSHA256,
+				tls.PSSWithSHA384,
+				tls.PSSWithSHA512,
+				tls.PKCS1WithSHA256,
+				tls.PKCS1WithSHA384,
+				tls.PKCS1WithSHA512,
+				tls.ECDSAWithSHA1,
+				tls.PKCS1WithSHA1,
+			},
+		},
+		"16": &tls.ALPNExtension{
+			AlpnProtocols: []string{"h2", "http/1.1"},
+		},
+		"18": &tls.SCTExtension{},
+		"21": &tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle},
+		"22": &tls.GenericExtension{Id: 22}, // encrypt_then_mac
+		"23": &tls.UtlsExtendedMasterSecretExtension{},
+		"27": &tls.FakeCertCompressionAlgsExtension{
+			Methods: []tls.CertCompressionAlgo{tls.CertCompressionBrotli},
+		},
+		"28": &tls.FakeRecordSizeLimitExtension{}, //Limit: 0x4001
+		"35": &tls.SessionTicketExtension{},
+		"34": &tls.GenericExtension{Id: 34},
+		"41": &tls.GenericExtension{Id: 41}, //FIXME pre_shared_key
+		"43": &tls.SupportedVersionsExtension{Versions: []uint16{
+			tls.GREASE_PLACEHOLDER,
+			tls.VersionTLS13,
+			tls.VersionTLS12,
+			tls.VersionTLS11,
+			tls.VersionTLS10}},
+		"44": &tls.CookieExtension{},
+		"45": &tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+			tls.PskModeDHE,
+		}},
+		"49": &tls.GenericExtension{Id: 49}, // post_handshake_auth
+		"50": &tls.GenericExtension{Id: 50}, // signature_algorithms_cert
+		"51": &tls.KeyShareExtension{KeyShares: []tls.KeyShare{
+			{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
+			{Group: tls.X25519},
+
+			// {Group: utls.CurveP384}, known bug missing correct extensions for handshake
+		}},
+		"30032": &tls.GenericExtension{Id: 0x7550, Data: []byte{0}}, //FIXME
+		"13172": &tls.NPNExtension{},
+		//"17513": &tls.ApplicationSettingsExtension{
+		//	SupportedALPNList: []string{
+	//			"h2",
+//			},
+	//	},
+		"65281": &tls.RenegotiationInfoExtension{
+			Renegotiation: tls.RenegotiateOnceAsClient,
+		},
+	}
+	return
+
+}
+
 func genMap() (extMap map[string]tls.TLSExtension) {
 	extMap = map[string]tls.TLSExtension{
 		"0": &tls.SNIExtension{},
